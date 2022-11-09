7d322b0d120013690bff8cb820641ecdfeee0094
MuratO.Edutec <muratedutec@gmail.com>
2022-11-09 19:22:02 +0100

    32.gundevam

--- src/ders32_inheritanceConstructorCall/Corolla.java
+++ src/ders32_inheritanceConstructorCall/Corolla.java
@@ -1,12 +1,45 @@
 package ders32_inheritanceConstructorCall;
 
 public class Corolla extends BToyota{
+    Corolla(int pc){
+        super(5);
+        System.out.println("Parametreli Corolla constructor");
+
+
+    }
+
+    Corolla(){
+        //System.out.println("Parametresiz Corolla constructor");
+    }
+
+    Corolla(String str){
+        this();
+        System.out.println("String parametreli Corolla constructor");
+
+    }
+
+
 
     public static void main(String[] args) {
 
-    Corolla corolla1=new Corolla();
+        Corolla corolla3=new Corolla("Ilker");
 
 
+
+
+
+
+
+        System.out.println("==========================================");
+        Corolla corolla1=new Corolla();
+        //Parametresiz Araba constructor
+        // Parametresiz Toyota constructor
+
+        System.out.println("==========================================");
+        Corolla corolla2=new Corolla(3);
+        //Parametresiz Araba constructor
+        //Parametresiz Toyota constructor
+        //String parametreli Corolla constructor
 
 
 
