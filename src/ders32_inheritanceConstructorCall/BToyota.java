ce38ccbf2594bcc5c36f51bfa3d5fb95130423a7
MuratO.Edutec <muratedutec@gmail.com>
2022-11-09 18:56:30 +0100

    32.gun

---
+++ src/ders32_inheritanceConstructorCall/BToyota.java
@@ -0,0 +1,11 @@
+package ders32_inheritanceConstructorCall;
+
+public class BToyota extends Araba{
+    BToyota(){
+        System.out.println("Parametresiz Toyota constructor");
+    }
+    BToyota (int pt){
+        System.out.println("Parametreli Toyota constructor");
+
+    }
+}
---
+++ src/ders32_inheritanceConstructorCall/Corolla.java
@@ -0,0 +1,16 @@
+package ders32_inheritanceConstructorCall;
+
+public class Corolla extends BToyota{
+
+    public static void main(String[] args) {
+
+    Corolla corolla1=new Corolla();
+
+
+
+
+
+    }
+
+
+}
