aa55ca62cb8e74875442ecf59c85ae6289097cfb
MuratO.Edutec <muratedutec@gmail.com>
2022-11-10 19:56:55 +0100

    33.gun baslsdik

---
+++ src/ders33_inheritanceDataTypeKullanimi_Overriding/FAvciKuslar.java
@@ -0,0 +1,21 @@
+package ders33_inheritanceDataTypeKullanimi_Overriding;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class FAvciKuslar extends EKuslar{
+    public void hareket (){
+        System.out.println("Avci kuslar ucarlar");
+    }
+    public void beslenme (){
+        System.out.println("Avci kuslar et yerler");
+    }
+    public void pence(){
+        System.out.println("Avci kuslar pencelidir");
+    }
+    public void gaga(){
+        System.out.println("Avci kuslar sivri gagalidir");
+    }
+
+
+
+}
