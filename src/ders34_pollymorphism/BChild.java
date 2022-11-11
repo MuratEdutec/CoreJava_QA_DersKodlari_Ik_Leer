1ab7c61a60a8b4bb35f6204bc6fbc6b8b9d51ac6
MuratO.Edutec <muratedutec@gmail.com>
2022-11-11 19:21:25 +0100

    34.gunbasladik

---
+++ src/ders34_pollymorphism/BChild.java
@@ -0,0 +1,40 @@
+package ders34_pollymorphism;
+
+public class BChild extends AParent{
+    public void method1(){
+        System.out.println("child class method1 ");
+
+    }
+    public void method2(int a){
+
+        //overriding olabilmesi icin
+        // hem method ismi hem de signature ayni olmalidir
+
+
+        System.out.println("Child class method2 ");
+
+    }
+
+    public void method2(String isim){
+        System.out.println("Child class String parametrerli method2 ");
+
+    }
+    @Override
+    public void method3(String str) {
+
+    //super.method3(str);
+    // Overriding varsa parent ve child class'daki
+        //overridden ve overiding method'lardan sadece biri calisir
+        //eger ikisini birden calistirmak isterseniz
+        // super.overiddenMethoddIsmi yazilir
+
+        /*
+        Overriding method @Override notasyonu ile isaretlenebilir
+         */
+
+    //
+}
+
+
+}
+
