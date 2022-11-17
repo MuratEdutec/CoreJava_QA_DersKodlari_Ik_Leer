2d2d1ed180615c414cfb4ddb49942e2e5b0fe011
MuratO.Edutec <muratedutec@gmail.com>
2022-11-17 21:18:54 +0100

    38.gunsonu

--- src/ders38_AbstractClasses/FAbstractChildClassOfAbstractClass.java
+++ src/ders38_AbstractClasses/FAbstractChildClassOfAbstractClass.java
@@ -2,5 +2,10 @@ package ders38_AbstractClasses;
 
 public abstract class FAbstractChildClassOfAbstractClass extends CParentAbstractClass{
 
+        public abstract void mecburiMethod1ChildAbstract();
+
+
+
+        public abstract void mecburiMethod2ChildAbstract();
 
 }
---
+++ src/ders38_AbstractClasses/GrandChildOfAbstractClasses.java
@@ -0,0 +1,30 @@
+package ders38_AbstractClasses;
+
+public class GrandChildOfAbstractClasses extends FAbstractChildClassOfAbstractClass{
+
+
+    // Abstract bir silsileden sonra gelen
+    // ilk concrete class
+    // atalarindaki concrete yapilmayan tum abstract methodlari
+    // IMPLEMENT ETMEK ZORUNDADIR
+
+    @Override
+    public void mecburiMethod1() {
+
+    }
+
+    @Override
+    public void mecburiMethod2() {
+
+    }
+
+    @Override
+    public void mecburiMethod1ChildAbstract() {
+
+    }
+
+    @Override
+    public void mecburiMethod2ChildAbstract() {
+
+    }
+}
