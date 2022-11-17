345a387d3b0e76a63fddcd42e07d2a825caacea8
MuratO.Edutec <muratedutec@gmail.com>
2022-11-17 20:24:05 +0100

    38.gundevamediyor

--- src/ders38_AbstractClasses/BChild.java
+++ src/ders38_AbstractClasses/BChild.java
@@ -2,8 +2,16 @@ package ders38_AbstractClasses;
 
 public class BChild extends AParent {
 
+/*
+Bugune kadar gordugumuz class'larda
+extends keywoord ile bir class'i parent edinirsek
+parent class'lardaki method'lari
+override etme MECBURIYETIMIZ yoktur,
 
-
+istersek parent class'taki tum methodlari override ederiz
+istersek de parent class'daki tum method'lari kullanir
+hicbirini override etmeyiz
+ */
 
 
 
