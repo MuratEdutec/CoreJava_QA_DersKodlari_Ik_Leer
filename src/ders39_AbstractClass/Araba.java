79503f73822abd0379fb3347fd003b1ae029a99e
MuratO.Edutec <muratedutec@gmail.com>
2022-11-18 19:17:41 +0100

    39.gun devam ediyor

--- src/ders39_AbstractClass/Araba.java
+++ src/ders39_AbstractClass/Araba.java
@@ -1,13 +1,19 @@
 package ders39_AbstractClass;
 
-public class Araba {
+public abstract class Araba {
+        // Araba class'ini inherit eden tum class'lar
+        // motor, kasa ve tekerlek method'larini override etmek zorunda kalsin
+    public abstract void motor();
 
-    public void motor(){    }
+    public  abstract void kasa();
 
-    public  void kasa(){    }
+    public abstract void tekerlek();
+    public void abs(){
+        System.out.println("Guvenlik artirmak icin abs takin ");
+    }
 
-    public void tekerlek(){ }
-
-
+    public void klima(){
+        System.out.println("Konfor artirmak icin klima takin ");
+    }
 
 }
--- src/ders39_AbstractClass/BHonda.java
+++ src/ders39_AbstractClass/BHonda.java
@@ -1,4 +1,22 @@
 package ders39_AbstractClass;
 
-public class BHonda extends Araba{
+public abstract class BHonda extends Araba{
+
+    //Abstract bir class'i parent edindigimizde
+    //iki alternatifimiz olur
+    // 1- Parent'imiz olan abstract class'daki abstract method'lari override etmek
+    // 2- proje yapimiz gerektiriyorsa bu class'i da abstract yapmak
+
+
+    public void marka(){
+        System.out.println("marka : Honda ");
+    }
+
+    public abstract void yakit();
+    public abstract void guvenlik();
+
+    public void ozelTeknoloji(){
+        System.out.println("Tum Honda arabalar Vtec teknolojisi kullanir ");
+    }
+
 }
