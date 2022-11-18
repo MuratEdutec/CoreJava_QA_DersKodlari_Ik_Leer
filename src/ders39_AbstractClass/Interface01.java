0667ecdea3dd166c2549f5dd73efe8aaf25aae92
MuratO.Edutec <muratedutec@gmail.com>
2022-11-18 21:23:59 +0100

    39.gun sonu

---
+++ src/ders39_AbstractClass/Interface01.java
@@ -0,0 +1,13 @@
+package ders39_AbstractClass;
+
+public interface Interface01 {
+
+    // interface'ler full abstraction saglarlar
+    // yani interface icerisinde concrete method bulundmaz
+
+    // interface'ler kendilerini parent kabul edecek
+    // child class'lar icin bir blue-print (sablon) gibidir
+
+
+
+}
---
+++ src/ders39_AbstractClass/KChildClass.java
@@ -0,0 +1,4 @@
+package ders39_AbstractClass;
+
+public class KChildClass extends BToyota implements Interface01,Interface02{
+}
