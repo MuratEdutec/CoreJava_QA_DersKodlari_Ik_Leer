4cee803dbd9305ba6a0cd0701a227ade1b260ef9
MuratO.Edutec <muratedutec@gmail.com>
2022-11-22 21:14:51 +0100

    42.gun devam ediyor

--- src/ders42_connections/C04_LinkedListQueueDataTuru.java
+++ src/ders42_connections/C04_LinkedListQueueDataTuru.java
@@ -21,5 +21,42 @@ public class C04_LinkedListQueueDataTuru {
         System.out.println(q1.remove());//parametre girmedigimizde bastaki elemani remove ettigi elemani dondurur
         System.out.println(q1);
 
+        System.out.println("__________");
+        System.out.println(q1.remove("K"));//true
+        System.out.println(q1);//[Y]
+
+        System.out.println("-__________");
+        q1.remove();  // bos queue kaldi []
+        System.out.println(q1);//[]
+        //q1.remove();// Tekrar yazilirsa ---queue bos oldugundan NoSuchElementException verir
+
+        System.out.println("__________");
+
+        System.out.println(q1.poll());
+        // .poll(); remove ile aynı işlemi yapar , yan bastakı elementi siler bize
+        // dondurur aralarındaki fark bos Queue 'da calıstırıldıklarında remove exception
+        // fırlatırken poll() exceptıon fırlatmaz.
+
+        q1.add("H");
+        q1.add("Y"); // [H, Y]
+
+        System.out.println(q1.peek());
+        // silmeden queue'nun basindaki elementi bize getiriyor
+        // eger queue bossa null getiriyor
+
+        System.out.println(q1);
+        System.out.println("__________");
+
+        System.out.println("element : " + q1.element());
+        System.out.println(q1);
+        System.out.println("__________");
+        q1.clear();// queue'yi bosalttik
+
+        System.out.println("peek : "+ q1.peek()); //null
+
+        //System.out.println("element : " + q1.element()); // exception
+
+
+
     }
 }
