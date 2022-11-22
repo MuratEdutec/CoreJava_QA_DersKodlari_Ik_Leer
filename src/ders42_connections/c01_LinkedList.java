25b09ac97f0265916c4e5b6adad1e4d999e1a564
MuratO.Edutec <muratedutec@gmail.com>
2022-11-22 20:15:40 +0100

    42.gun devam ediyor

--- src/ders42_connections/c01_LinkedList.java
+++ src/ders42_connections/c01_LinkedList.java
@@ -3,13 +3,13 @@ package ders42_connections;
 import java.util.LinkedList;
 import java.util.List;
 
-public class d01_LinkedList {
+public class c01_LinkedList {
     public static void main(String[] args) {
 
 
         LinkedList<String> ll1= new LinkedList<>();
 
-        ll1.add("I");
+        ll1.add("K");
         ll1.add("T");
 
 
@@ -25,7 +25,7 @@ public class d01_LinkedList {
         ll2.add("R");//[R]
         System.out.println(ll2);
 
-        ll2.add("A");//[R, A] --- sona ekler
+        ll2.add("Z");//[R, Z] --- sona ekler
         System.out.println(ll2);
 
         ll2.add(0,"A");
@@ -36,13 +36,18 @@ public class d01_LinkedList {
 
         ll2.set(3,"M");
         System.out.println(ll2);//[A, R, I, M, A]
-
+        ll2.add("K");
+        ll2.add("T");
 
         System.out.println(ll2.get(1));//R
 
         System.out.println(ll2.retainAll(ll1));
         System.out.println(ll2);
-        System.out.println(ll1);
+        ll2.add("A");
+        System.out.println(ll2.hashCode());//3232590
+
+        System.out.println(ll2.subList(2, 4));//[K,K,T,A] 2 dahil, 4 dahil olmadigindan exception firlatmaz
+
 
 
     }
