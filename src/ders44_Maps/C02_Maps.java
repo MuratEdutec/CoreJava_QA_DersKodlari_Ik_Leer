375cebc2cbc9a259532283a3f59e6e8b5d2ca618
MuratO.Edutec <muratedutec@gmail.com>
2022-11-24 20:25:44 +0100

    44.gun Mapssss

--- src/ders44_Maps/C02_Maps.java
+++ src/ders44_Maps/C02_Maps.java
@@ -1,8 +1,33 @@
 package ders44_Maps;
 
+import java.util.Map;
+
 public class C02_Maps {
 
     public static void main(String[] args) {
+
+    Map<Integer, String> ogrenciMap= MapDepo.ornekMapOlustur();
+
+    ogrenciMap.put(105, "Hasan-Can-11-K-MF");
+    ogrenciMap.put(108, "Hikmet-Han-10-M-TM");
+
+        System.out.println(ogrenciMap);
+            /*
+            Eger map'te olan bir key ile yeni bir deger eklerseniz
+            O key'e ait olan value silinir ve yeni value gecerli olur
+
+            {101=Ali-Can-10-H-MF,
+            102=Veli-Cem-11-M-Soz,
+            103=Ali-Cem-11-H-TM,
+            104=Ayse-Can-10-H-MF,
+            105=Hasan-Can-11-K-MF, ****
+            106=Fatma-Han-10-K-Soz,
+            108=Hikmet-Han-10-M-TM
+            }
+
+             */
+
+
 
     }
 
