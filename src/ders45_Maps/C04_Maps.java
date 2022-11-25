a74d9e84a44a19dadb2b5e4317e76684f08a3219
MuratO.Edutec <muratedutec@gmail.com>
2022-11-25 21:04:57 +0100

    45.gunMaps

--- src/ders45_Maps/C04_Maps.java
+++ src/ders45_Maps/C04_Maps.java
@@ -13,8 +13,10 @@ public class C04_Maps {
 
         Map<Integer, String> ogrenciMap=MapDepo.ornekMapOlustur();
 
-        int basNo= 103;
-        int bitNo=107;
+        ogrenciMap.put(107,"Tugay-Cil-12-K-MF");
+        ogrenciMap.put(103,"Hikmet-Saglam-11-K-Soz");
+        int basNo= 102;
+        int bitNo= 120;
         List<String> isimListesi= MapDepo.numaraDegerineGoreListe(ogrenciMap, basNo, bitNo);
         System.out.println(isimListesi);
 
---
+++ src/ders45_Maps/C05_EntrySet.java
@@ -0,0 +1,17 @@
+package ders45_Maps;
+
+import ders44_Maps.MapDepo;
+
+import java.util.Map;
+
+public class C05_EntrySet {
+    public static void main(String[] args) {
+
+        // Ogrenci Map'inden Numara, isim ve soyisim degerlerini yazdiralim
+
+
+
+
+        Map<Integer, String> ogrenciMap= MapDepo.ornekMapOlustur();
+    }
+}
